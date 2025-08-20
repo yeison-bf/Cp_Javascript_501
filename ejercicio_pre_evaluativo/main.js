3278eff0bada3f3efe453a32476028edcf3e442c
yeison-bf <yeison.barrios.funieles@gmail.com>
2025-08-20 07:16:05 -0500

    ok

--- ejercicio_pre_evaluativo/main.js
+++ ejercicio_pre_evaluativo/main.js
@@ -1,6 +1,17 @@
 // Variables y arreglos globales
 listadoEmpleados = []
-listadoEmpresas = []
+listadoEmpresas = [
+    {
+        nit: 1051891891,
+        razonSocial: "SENA",
+        direccion: "Medellin"
+    },
+    {
+        nit: 1051892892,
+        razonSocial: "Bfunieles",
+        direccion: "Medellin"
+    }
+]
 
 const InicioDesdeAqui = () => {
     let opcion = 0;
@@ -33,23 +44,10 @@ const MenuEmpresa = () => {
         opcionEmpresa = parseInt(prompt("GESTIÓN DE EMPRESAS\n1. Registro\n2.Listado\n3. Atras"))
         switch (opcionEmpresa) {
             case 1:
-                let cantidad = parseInt(prompt(`¿Cuantos registros deseas realizar?`))
-                for (let i = 1; i <= cantidad; i++) {
-                    listadoEmpresas.push({
-                        nit: parseInt(prompt("NIT")),
-                        razonSocial: prompt("Razón social"),
-                        direccion: prompt("Dirección")
-                    })
-                }
+                registroEmpresas()
                 break;
             case 2:
-                alert("Listadio de empresas")
-                for (let i = 0; i < listadoEmpresas.length; i++) {
-                     alert(`INFORMACIÓN DE LA EMPRESA:
-                        NIT: ${listadoEmpresas[i].nit}
-                        Razon social: ${listadoEmpresas[i].razonSocial}
-                        Dirección: ${listadoEmpresas[i].direccion}`)
-                }
+                listadoDatosEmpresas()
                 break;
             case 3:
                 break;
@@ -59,6 +57,50 @@ const MenuEmpresa = () => {
 
         }
     } while (opcionEmpresa !== 3)
+}
+
+// Función para registrar una empresa
+const registroEmpresas = () => {
+    let cantidad = parseInt(prompt(`¿Cuantos registros deseas realizar?`))
+    for (let i = 1; i <= cantidad; i++) {
+        listadoEmpresas.push({
+            nit: parseInt(prompt("NIT")),
+            razonSocial: prompt("Razón social"),
+            direccion: prompt("Dirección")
+        })
+    }
+}
+
+// Función para listar las empresas
+const listadoDatosEmpresas = () => {
+    
+    // alert("Listadio de empresas")
+    // for (let i = 0; i < listadoEmpresas.length; i++) {
+    //     alert(`INFORMACIÓN DE LA EMPRESA:
+    //                     NIT: ${listadoEmpresas[i].nit}
+    //                     Razon social: ${listadoEmpresas[i].razonSocial}
+    //                     Dirección: ${listadoEmpresas[i].direccion}`)
+    // }
+
+    let dataEmpresa = ""
+    listadoEmpresas.forEach((element, index)=>{
+        dataEmpresa += `
+        Empresa No. ${(index+1)}
+        Nit: ${element.nit}
+        Nombre: ${element.razonSocial}
+        Dirección: ${element.direccion}\n`
+    })
+
+    alert(dataEmpresa)
+}
+
+// Función para listar empresas en el formulario de empleados
+const datosEmpresaParaEmpleados = () => {
+    let DataEmpresa = "";
+    for (let i = 0; i < listadoEmpresas.length; i++) {
+        DataEmpresa += `${i} : NIT: ${listadoEmpresas[i].nit} - Razon social: ${listadoEmpresas[i].razonSocial}\n`
+    }
+    return DataEmpresa
 }
 
 
@@ -73,9 +115,34 @@ const MenuEmpleados = () => {
         switch (opcionEmpleados) {
             case 1:
                 alert("Registro de Empleados")
+                cantidad = parseInt(prompt("¿Cuantos registros deseas hacer?"))
+                for (let i = 0; i < cantidad; i++) {
+
+                    let documento = parseInt(prompt("Documento"))
+                    let nombre = prompt("Nombre")
+                    let apellidos = prompt("Apellidos")
+                    let direccion = prompt("Direccion")
+                    let usuario = prompt("Usuario de acceso")
+                    let password = prompt("Password")
+                    let idEmpresa = parseInt(prompt(`Seleccione la empresa\n ${datosEmpresaParaEmpleados()}`))
+
+                    listadoEmpleados.push({
+                        documento, nombre, apellidos, direccion, usuario, password, idEmpresa
+                    })
+                }
                 break;
             case 2:
                 alert("Listado de Empleados")
+                let dataEmpleados = "";
+                for (let i = 0; i < listadoEmpleados.length; i++) {
+                    dataEmpleados += `
+                        Nombre: ${listadoEmpleados[i].nombre}
+                        Apellidos: ${listadoEmpleados[i].apellidos}
+                        Direccion: ${listadoEmpleados[i].direccion}
+                        Empresa : ${listadoEmpresas[listadoEmpleados[i].idEmpresa].razonSocial}\n`
+                }
+
+                alert(dataEmpleados)
                 break;
             case 3:
                 break;
